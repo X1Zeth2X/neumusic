3abce025d934fba38fb2d65ce7d90420ab4a32d2
Zeth Leonardo <zeth.leonardo@protonmail.com>
2021-06-05 03:30:10 +0400

    Added guild commands.

---
+++ src/events/guild/message.ts
@@ -0,0 +1,37 @@
+import { ExecuteFunction } from "../../interfaces/event";
+import { Message } from "discord.js";
+import Command from "../../interfaces/command";
+
+export const execute: ExecuteFunction = async (client, message: Message) => {
+  if (
+    message.author.bot ||
+    !message.guild ||
+    !message.content.toLowerCase().startsWith("nm!")
+  )
+    return;
+
+  const args: Array<string> = message.content
+    .slice("nm!".length)
+    .trim()
+    .split(/ +/g);
+
+  // @ts-ignore
+  const argCommand: string = args.shift();
+  // @ts-ignore
+  const command: Command = client.commands.get(argCommand);
+
+  if (!command) return;
+
+  command.execute(client, message, args).catch((reason: any) =>
+    message.channel.send(
+      client.embed(
+        {
+          description: `An error occured during execution: ${reason}`,
+        },
+        message
+      )
+    )
+  );
+};
+
+export const name: string = "message";
