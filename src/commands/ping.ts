8dd1db4c6913ae27318648a4b530342e7752f9f6
Zeth Leonardo <zeth.leonardo@protonmail.com>
2021-06-05 05:02:59 +0400

    Added basic music functionalities.

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -5,6 +5,8 @@ export const execute: ExecuteFunction = async (client, message) => {
   const msg: Message = await message.channel.send(
     client.embed({ description: "PONG!" }, message)
   );
+
+  client.logger.log(`Bot sent a message: ${msg}`);
 };
 
 export const name: string = "ping";
