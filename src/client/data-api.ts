02200783451b98e6d556eac65e2114679ed3baa5
Zeth Leonardo <zeth.leonardo@protonmail.com>
2021-06-09 14:31:25 +0400

    Added loadItems function and types.

--- src/client/data-api.ts
+++ src/client/data-api.ts
@@ -1,4 +1,5 @@
 import fetch from "node-fetch";
+import ytdl, { MoreVideoDetails, VideoDetails } from "ytdl-core";
 
 class YTClient {
   url: string;
@@ -11,10 +12,69 @@ class YTClient {
 
   async makeRequest(params: string) {
     const url: string = `${this.url}${params}&key=${this.key}`;
-    console.log(`Performed fetch request using: ${url}`);
-
     return await fetch(url);
+  }
+
+  loadItems(items: Array<Item>): Promise<VideoData[]> {
+    return new Promise<VideoData[]>(async (resolve, _reject) => {
+      const videosData: Array<VideoData> = [];
+
+      for (const video of items) {
+        // Get the ID of the video
+        const videoId: string = video.id.videoId;
+
+        // Request video details w/ytdl
+        const videoDetails: MoreVideoDetails = (
+          await ytdl.getBasicInfo(videoId)
+        ).videoDetails;
+
+        const videoData: VideoData = {
+          videoTitle: videoDetails.title,
+          videoDesc: `${videoDetails.description?.substring(0, 100)}...`,
+          videoThumbnail: videoDetails.thumbnails[2].url,
+          videoUrl: videoDetails.video_url,
+        };
+
+        videosData.push(videoData);
+      }
+
+      resolve(videosData);
+    });
   }
 }
 
+type YTResponse = {
+  kind: string;
+  etag: string;
+  nextPageToken?: string;
+  prevPageToken?: string;
+  regionCode: string;
+  pageInfo: PageInfo;
+  items: Array<Item>;
+};
+
+type PageInfo = {
+  totalResults: number;
+  resultsPerPage: number;
+};
+
+type Item = {
+  kind: string;
+  etag: string;
+  id: Id;
+};
+
+type Id = {
+  kind: string;
+  videoId: string;
+};
+
+type VideoData = {
+  videoTitle: string;
+  videoDesc?: string;
+  videoThumbnail: string;
+  videoUrl: string;
+};
+
+export { YTResponse, YTClient };
 export default YTClient;
