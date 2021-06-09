fadf00ed18c2b66873e8f21a3102cbda8dc0ea10
Zeth Leonardo <zeth.leonardo@protonmail.com>
2021-06-09 21:06:05 +0400

    Added help command.

---
+++ src/commands/help.ts
@@ -0,0 +1,21 @@
+import { ExecuteFunction } from "../interfaces/command";
+
+export const name: string = "help";
+
+export const execute: ExecuteFunction = async (client, message) => {
+  const avatarUrl = client.user?.avatarURL({ format: 'png', dynamic: true });
+
+  await message.channel.send(
+    client.embed(
+      {
+        title: "Help",
+        url: "https://github.com/X1Zeth2X/neumusic",
+        thumbnail: {
+          url: avatarUrl ? avatarUrl : ''
+        },
+        description: "Commands can be found on the GitHub, click on the title.",
+      },
+      message
+    )
+  );
+};
