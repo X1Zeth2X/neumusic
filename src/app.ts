5b4652bcefe866f6347fa011b93223338e279837
Zeth Leonardo <zeth.leonardo@protonmail.com>
2021-06-09 22:14:13 +0400

    Added express for web app hosting.

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,16 @@
 import "dotenv/config";
 import NeumusicBot from "./client/bot";
+import express from "express";
 
-new NeumusicBot().run(process.env.TOKEN);
+const server = express();
+const port = process.env.PORT || "3000";
+
+server.all("/", (_req, res) => {
+  res.send("We're live :)");
+});
+
+server.listen(port, () => {
+  // Instantiate the bot and run it
+  new NeumusicBot().run(process.env.TOKEN);
+  console.log("Server ready.");
+});
