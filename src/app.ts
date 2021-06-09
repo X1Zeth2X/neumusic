68e8ae86f69daec5962bc7a2b7f2fec0d7ff0438
Zeth Leonardo <zeth.leonardo@protonmail.com>
2021-06-09 21:39:13 +0400

    Add useless port to prevent Heroku crash.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
-import discordButtons from "discord-buttons";
 import "dotenv/config";
 import NeumusicBot from "./client/bot";
 
-const client = new NeumusicBot();
 
-// Bind Discord Buttons and Run App.
-discordButtons(client);
-client.run(process.env.TOKEN);
+const port = process.env.PORT;
+new NeumusicBot().run(process.env.TOKEN);
+
+console.log(`Useless Port :${port}`);
