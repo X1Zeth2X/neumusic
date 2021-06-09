12d1c491c6857871f9d4bac9565147e57c03e7fa
Zeth Leonardo <zeth.leonardo@protonmail.com>
2021-06-09 19:56:25 +0400

    Added url playing support.

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -3,9 +3,10 @@ import MusicUtil from "../utils/voice.handler";
 
 export const name: string = "play";
 
-export const execute: ExecuteFunction = async (_client, message) => {
+export const execute: ExecuteFunction = async (_client, message, ...args) => {
   const MUtil = new MusicUtil(message, name);
-  const response = await MUtil.play();
+  const url: string = args[0][0];
+  const response = await MUtil.play(url);
 
   if (response) message.channel.send(response);
 };
